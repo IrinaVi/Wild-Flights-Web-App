11a16d95060dd7c713de27a87ac6d28bc23d1f14
IrinaVi <irinavik1805@gmail.com>
2022-03-24 18:23:02 +0000

    changed postgres to postgresql

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bzfeospvnoyndy:0cefb46f528415a94f99a32fccf3a2285f42d3903c786a8c308b472da637bf7c@ec2-3-229-161-70.compute-1.amazonaws.com:5432/dempo515nlcd7f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bzfeospvnoyndy:0cefb46f528415a94f99a32fccf3a2285f42d3903c786a8c308b472da637bf7c@ec2-3-229-161-70.compute-1.amazonaws.com:5432/dempo515nlcd7f'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flights-users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
